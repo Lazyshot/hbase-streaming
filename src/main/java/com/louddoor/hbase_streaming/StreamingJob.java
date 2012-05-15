f4414c9f70e6522ba4fb404ec0792efceae76ac7
Bryan <lazyshot@gmail.com>
2012-05-15 10:22:04 -0400

    adding asl 2.0

--- src/main/java/com/louddoor/hbase_streaming/StreamingJob.java
+++ src/main/java/com/louddoor/hbase_streaming/StreamingJob.java
@@ -30,7 +30,6 @@ import org.apache.hadoop.hbase.mapreduce.TableMapper;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
